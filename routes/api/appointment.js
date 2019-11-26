66452596fb8beafeb2c1e42e7785bc90db80bbb8
TargaryenQi <44879154+TargaryenQi@users.noreply.github.com>
2019-11-26 09:07:46 -0500

    fix appointment api to adjust to the User model change to Google auth

--- routes/api/appointment.js
+++ routes/api/appointment.js
@@ -1,91 +1,116 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../../middleware/auth');
-const { check, validationResult } = require('express-validator');
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-const User = require('../../models/User');
-const Appointment = require('../../models/Appointment')
+const User = require("../../models/User");
+const Appointment = require("../../models/Appointment");
 
 // @route  POST api/appointment
 // @desc   Create an appointment
 // @access Private
-router.post('/',[check('title','Service is required').not().isEmpty(),check('date','Date is required')],async (req,res)=>{
+router.post(
+  "/",
+  [
+    check("title", "Service is required")
+      .not()
+      .isEmpty(),
+    check("date", "Date is required")
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  if(!errors.isEmpty()) {
-    return res.status(400).json({errors:errors.array()})
-  }
-
-  const {name,email,phone,staff,staff_email,title,price,date,time,duration} = req.body;
-
-  try {
-    const newAppointment = new Appointment({
+    const {
       name,
+      email,
       phone,
       staff,
       staff_email,
-      email,
       title,
       price,
       date,
       time,
       duration
-    })
+    } = req.body;
 
-    const appointment = await newAppointment.save();
+    try {
+      const newAppointment = new Appointment({
+        name,
+        phone,
+        staff,
+        staff_email,
+        email,
+        title,
+        price,
+        date,
+        time,
+        duration
+      });
 
-    res.json(appointment);
-  } catch(err) {
-    console.log(err.message);
-    res.status('500').send('Server Error');
+      const appointment = await newAppointment.save();
+
+      res.json(appointment);
+    } catch (err) {
+      console.log(err.message);
+      res.status("500").send("Server Error");
+    }
   }
-})
+);
 
 // @route GET api/appointment
 // @desc  GET all appointments
 // @access Private
 
-router.get('/',auth,async (req,res) => {
+router.get("/", auth, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select('-password')
+    const user = await User.findById(req.user.id).select("-password");
 
-    const {isDoctor,email} = user;
-    let appointments = [];
-    if(isDoctor) {
-      appointments = await Appointment.find({staff_email:email})
+    const { isDoctor, method } = user;
+    let email;
+    if (method === "local") {
+      email = user.local.email;
     } else {
-      appointments = await Appointment.find({email:email})
+      email = user.google.email;
+    }
+    let appointments = [];
+    if (isDoctor) {
+      appointments = await Appointment.find({ staff_email: email });
+    } else {
+      appointments = await Appointment.find({ email: email });
     }
 
     res.json(appointments);
-  } catch(err) {
+  } catch (err) {
     console.log(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
-})
+});
 
 // @route GET api/appointment/:id
 // @desc  Get post by ID
 // @access Private
-router.get('/:id',auth,async(req,res)=> {
+router.get("/:id", auth, async (req, res) => {
   try {
     const appointment = await Appointment.findById(req.params.id);
 
-    if(!appointment) {
-      return res.status(400).json({msg:'Appointment not found'})
+    if (!appointment) {
+      return res.status(400).json({ msg: "Appointment not found" });
     }
 
     res.json(appointment);
-  } catch(err) {
+  } catch (err) {
     console.log(err.message);
-    if(err.kind === 'ObjectId') {
+    if (err.kind === "ObjectId") {
       return res.status(404).json({
-        msg:'Post not found'
+        msg: "Post not found"
       });
     }
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
-})
+});
 
 module.exports = router;
